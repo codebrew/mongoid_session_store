111123f136129c91c896ba9d7598a88a98042a9d
Ryan Fitzgerald <ryan@gymlogr.com>
2010-08-09 13:41:06 -0400

    Initial commit to mongoid_session_store.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'mongoid_session_store'
+
+class Test::Unit::TestCase
+end

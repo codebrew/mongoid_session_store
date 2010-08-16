916be2e041789d6918dad47418da4fc7965d7168
Ryan Fitzgerald <ryan@gymlogr.com>
2010-08-15 21:02:27 -0400

    setting up to run in production mode

--- example/config/initializers/session_store.rb
+++ example/config/initializers/session_store.rb
@@ -4,8 +4,4 @@ require 'mongoid_session_store/mongoid_store'
 
 # Example::Application.config.session_store :cookie_store, :key => '_example_session'
 Example::Application.config.session_store :mongoid_store
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
 # Example::Application.config.session_store :active_record_store
